1e07d6d5939b96fb9619abbd5ed4edb389186be8
Autumn Leaf <101070305+AomegaL@users.noreply.github.com>
2022-03-10 08:41:28 -0600

    Add files via upload

--- MAKE.bash
+++ MAKE.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# pyinstaller does all the work for us, compressing all the files into one py executable
+# pyinstaller does all the work for us, compressing
+# all the files into one py executable
+#
 # if you're a contributor, make sure to install upx
 pyinstaller --onefile *.py
 
-# we don't know what's in there, but we're assuming it's the file we built
-mkdir build
-mv dist/* build
+# what's likely in there is xrd masquerading as another
+# python file, so we're moving it
+mv dist/cmds dist/xrd
